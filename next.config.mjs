f8cc6163ebac14aa29d2df6dc57de8ac854c63b1
maram qais <maramqaiszebar@gmail.com>
2024-03-23 03:40:14 +0300

    implement responsive navbar and display movies on home page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+    reactStrictMode: false, 
+  };
+  
+  export default nextConfig;
 
-export default nextConfig;
+  
