f7685723c96061bd4572dbd3a78306421c601871
maram qais <maramqaiszebar@gmail.com>
2024-03-24 05:44:28 +0300

    updated page-layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,27 @@
-// Inside your Home.js or wherever your main component is
-import React from 'react';
+"use client"
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar'; 
 import HeroSection from "../components/HeroSection/HeroSection";
 import SectionSlider from '../components/Sections/SectionSlider'; 
+import Footer from '@/components/Footer';
+import Loader from '../components/Loader/Loader';
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true); 
   const apiKey = "729adca28d6a4301ad60d0d49fbaddde";
+
+  useEffect(() => {
+  
+    const timer = setTimeout(() => {
+      setIsLoading(false); 
+    }, 3000); 
+
+    return () => clearTimeout(timer); 
+  }, []); 
 
   return (
     <>
+      {isLoading && <Loader />} 
       <Navbar />
       <HeroSection />
       <SectionSlider 
@@ -27,10 +40,19 @@ const Home = () => {
         title="Now Playing" 
         fetchUrl={`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`} 
       />
-      <SectionSlider 
+      {/* <SectionSlider 
         title="Popular Actors" 
         fetchUrl={`https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=1`} 
-      />
+      /> */}
+
+<SectionSlider
+  title="Popular Actors"
+  fetchUrl={`https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=1`}
+  extraClass="actors-section"
+/>
+
+
+      <Footer />
     </>
   );
 };
